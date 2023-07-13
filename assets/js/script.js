ca470e290bd0423de0cef3ff18fe9f0ac8649ce7
DanilleH22 <>
2023-07-13 12:46:34 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnser() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion() {
+
+}
